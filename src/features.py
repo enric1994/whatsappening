1c83ae01d1a5e6c5825f0ed2cd3f54ac134ff4a9
Enric Moreu <enric.moreu.filella@gmail.com>
2021-02-09 22:48:08 +0000

    Feature clustering and sentiments

--- src/features.py
+++ src/features.py
@@ -1,17 +1,28 @@
+
+print('Importing NLP module...')
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
 import utils
 import os
 import csv
 from tqdm import tqdm
 from datetime import timedelta
+import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+import re
 
 
 
-INPUTS_PATH = os.path.join('..','data','small_chats')
+INPUTS_PATH = os.path.join('..','data','raw')
 OUTPUT_FILE = os.path.join('..','data','output','features.png')
+MIN_SENTENCE = 40
+START_DATE = '2020-08-01'
+END_DATE = '2021-01-01'
+DOT_SIZE = 7
 
 print('Loading model...')
 model = SentenceTransformer('paraphrase-xlm-r-multilingual-v1')
@@ -19,13 +30,10 @@ model = SentenceTransformer('paraphrase-xlm-r-multilingual-v1')
 
 chats = utils.list_chats(INPUTS_PATH)
 
-sentences_raw = []
+sentences = []
+chat_names = []
     
 for chat_name in chats:
-    print('#'*50)
-    print('#'*50)
-    print(chat_name)
-    print('-'*50)
     chat_path = os.path.join(INPUTS_PATH, chat_name)
     chat = utils.get_chat(chat_path)
 
@@ -37,29 +45,57 @@ for chat_name in chats:
     chat.df.drop(r)
 
     # Limit study period (2020-08-01) until (2021-00-00)
-    days_before = chat.df[(chat.df.date<= '2020-08-01')].index
+    days_before = chat.df[(chat.df.date<= START_DATE)].index
     aux_chat = chat.df.drop(days_before)
-    days_after = aux_chat[(aux_chat.date>= '2021-01-01')].index
+    days_after = aux_chat[(aux_chat.date>= END_DATE)].index
     chat_df = aux_chat.drop(days_after)
     
-
     # Parse sentences
     for m in chat_df.message:
-        # Only analyze sentences longer than 15 characters
-        [sentences_raw.extend(x.split('.')) for x in m.split('\n') if len(x) > 15]
+        # Only analyze long sentences and remove links
+        for l in m.split('\n'):
+            sentences_dot = l.split('.')
+            sentences_raw = [re.sub(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))', '', x) for x in sentences_dot if len(x) > MIN_SENTENCE]
+            sentences.extend(sentences_raw)
 
-sentences = [x for x in sentences_raw if len(x) > 15]
+            chat_names.extend([chat_name[:-4]] * len(sentences_raw))
 
 # Extract features
 print('Encoding {} sentences...'.format(len(sentences)))
 sentence_embeddings = model.encode(sentences, show_progress_bar=True)
 
-pca = PCA(n_components=2)
-pca.fit(sentence_embeddings)
-X = pca.transform(sentence_embeddings)
+# tsne = TSNE(n_components=2, verbose=1, perplexity=100, n_iter=1000)
+# X = tsne.fit_transform(sentence_embeddings)
+
+# import pdb;pdb.set_trace()
+# pca = PCA(n_components=2)
+# pca.fit(sentence_embeddings)
+# X = pca.transform(sentence_embeddings)
+# import pdb;pdb.set_trace()
+tsne = TSNE(n_components=2, verbose=1, perplexity=100, n_iter=1000)
+X = tsne.fit_transform(sentence_embeddings)
+
+
+
+data = pd.DataFrame(X, columns=['d1', 'd2'])
+data['chat'] = chat_names
 
 fig = plt.figure()
+# sns.color_palette("Set2")
+# sns.color_palette()
+# sns.color_palette("Paired")
 
-plt.scatter(X[:, 0], X[:, 1])
 
-fig.savefig(OUTPUT_FILE, dpi=fig.dpi)
+g = sns.scatterplot(data=data, x="d1", y="d2", hue="chat", palette="husl", s=DOT_SIZE, alpha=0.5)
+
+g.set_ylabel('')    
+g.set_xlabel('')
+
+handles, labels  =  g.get_legend_handles_labels()
+
+g.legend(handles, np.unique(chat_names), loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=5, prop={'size': 5})
+
+
+fig.savefig(OUTPUT_FILE, dpi=1000)
+
+print('done')
--- src/stats.py
+++ src/stats.py
@@ -3,222 +3,234 @@ import os
 import csv
 from datetime import timedelta
 from tqdm import tqdm
+from polyglot.text import Text
 
-INPUTS_PATH = os.path.join('..','data','raw')
+
+INPUTS_PATH = os.path.join('..','data','small_chats')
 OUTPUT_FILE = os.path.join('..','data','output','stats.csv')
-
+MIN_SENTENCE = 100
 
 with open(OUTPUT_FILE, 'w') as f:
-    csvf = csv.writer(f, quoting=csv.QUOTE_NONE, escapechar=' ')
-    csvf.writerow([
-        'Chat Name',
-        'Total Messages', 
-        'Average Characters Length', 
-        'Emoji Count',
-        'Start Date', 'End Date',
-        'Messages at 0 hours', 'Messages at 1 hours', 'Messages at 2 hours', 'Messages at 3 hours',
-        'Messages at 4 hours', 'Messages at 5 hours', 'Messages at 6 hours', 'Messages at 7 hours',
-        'Messages at 8 hours', 'Messages at 9 hours', 'Messages at 10 hours', 'Messages at 11 hours',
-        'Messages at 12 hours', 'Messages at 13 hours', 'Messages at 14 hours', 'Messages at 15 hours',
-        'Messages at 16 hours', 'Messages at 17 hours', 'Messages at 18 hours', 'Messages at 19 hours',
-        'Messages at 20 hours', 'Messages at 21 hours', 'Messages at 22 hours', 'Messages at 23 hours',
-        'Messages January', 'Messages February', 'Messages March', 'Messages April',
-        'Messages May', 'Messages June', 'Messages July', 'Messages August',
-        'Messages September', 'Messages October', 'Messages November', 'Messages December',
-        'Messages Monday', 'Messages Tuesday', 'Messages Wednesday', 'Messages Thursday',
-        'Messages Friday', 'Messages Saturday', 'Messages Sunday',
-        'Total breaks',
-        'Total exclamations!',
-        'Total interrogations?',
-        'Total capital letters',
-        'Total links',
-        'Total videos',
-        'Total images',
-        'Top 1 emoji',
-        'Top 1 emoji usage',
-        'Top 2 emoji',
-        'Top 2 emoji usage',
-        'Top 3 emoji',
-        'Top 3 emoji usage',
-        'Top 4 emoji',
-        'Top 4 emoji usage',
-        'Top 5 emoji',
-        'Top 5 emoji usage',
-        'Top 6 emoji',
-        'Top 6 emoji usage',
-        'Top 7 emoji',
-        'Top 7 emoji usage',
-        'Top 8 emoji',
-        'Top 8 emoji usage',
-        'Top 9 emoji',
-        'Top 9 emoji usage',
-        'Top 10 emoji',
-        'Top 10 emoji usage'
-        ])
+	csvf = csv.writer(f, quoting=csv.QUOTE_NONE, escapechar=' ')
+	csvf.writerow([
+		'Chat Name',
+		'Total Messages', 
+		'Average Characters Length', 
+		'Emoji Count',
+		'Start Date', 'End Date',
+		'Messages at 0 hours', 'Messages at 1 hours', 'Messages at 2 hours', 'Messages at 3 hours',
+		'Messages at 4 hours', 'Messages at 5 hours', 'Messages at 6 hours', 'Messages at 7 hours',
+		'Messages at 8 hours', 'Messages at 9 hours', 'Messages at 10 hours', 'Messages at 11 hours',
+		'Messages at 12 hours', 'Messages at 13 hours', 'Messages at 14 hours', 'Messages at 15 hours',
+		'Messages at 16 hours', 'Messages at 17 hours', 'Messages at 18 hours', 'Messages at 19 hours',
+		'Messages at 20 hours', 'Messages at 21 hours', 'Messages at 22 hours', 'Messages at 23 hours',
+		'Messages January', 'Messages February', 'Messages March', 'Messages April',
+		'Messages May', 'Messages June', 'Messages July', 'Messages August',
+		'Messages September', 'Messages October', 'Messages November', 'Messages December',
+		'Messages Monday', 'Messages Tuesday', 'Messages Wednesday', 'Messages Thursday',
+		'Messages Friday', 'Messages Saturday', 'Messages Sunday',
+		'Total breaks',
+		'Total exclamations!',
+		'Total interrogations?',
+		'Total capital letters',
+		'Total links',
+		'Total videos',
+		'Total images',
+		'Top 1 emoji',
+		'Top 1 emoji usage',
+		'Top 2 emoji',
+		'Top 2 emoji usage',
+		'Top 3 emoji',
+		'Top 3 emoji usage',
+		'Top 4 emoji',
+		'Top 4 emoji usage',
+		'Top 5 emoji',
+		'Top 5 emoji usage',
+		'Top 6 emoji',
+		'Top 6 emoji usage',
+		'Top 7 emoji',
+		'Top 7 emoji usage',
+		'Top 8 emoji',
+		'Top 8 emoji usage',
+		'Top 9 emoji',
+		'Top 9 emoji usage',
+		'Top 10 emoji',
+		'Top 10 emoji usage'
+		])
 
-    # Count total messages + start date + end date
-    chats = utils.list_chats(INPUTS_PATH)
-    for chat_name in chats:
-        print('#'*50)
-        print('#'*50)
-        print(chat_name)
-        print('-'*50)
-        chat_path = os.path.join(INPUTS_PATH, chat_name)
-        chat = utils.get_chat(chat_path)
+	# Count total messages + start date + end date
+	chats = utils.list_chats(INPUTS_PATH)
+	for chat_name in chats:
+		print('#'*50)
+		print('#'*50)
+		print(chat_name)
+		print('-'*50)
+		chat_path = os.path.join(INPUTS_PATH, chat_name)
+		chat = utils.get_chat(chat_path)
 
-        # Decrease the time 4 hours to compensate time shift France/Brazil:
-        chat.df['date'] -= timedelta(hours=4)
+		# Decrease the time 4 hours to compensate time shift France/Brazil:
+		chat.df['date'] -= timedelta(hours=4)
 
-        print('Chat start date: ', chat.start_date)
-        print('Chat end date: ', chat.end_date)
+		print('Chat start date: ', chat.start_date)
+		print('Chat end date: ', chat.end_date)
 
-        # Remove messages from user
-        r = chat.df[(chat.df.username=='Giuliander Carpes')].index
-        chat.df.drop(r)
+		# Remove messages from user
+		r = chat.df[(chat.df.username=='Giuliander Carpes')].index
+		chat.df.drop(r)
 
-        # Limit study period (2020-08-01) until (2021-00-00)
-        days_before = chat.df[(chat.df.date<= '2020-08-01')].index
-        aux_chat = chat.df.drop(days_before)
-        days_after = aux_chat[(aux_chat.date>= '2021-01-01')].index
-        chat_df = aux_chat.drop(days_after)
+		# Limit study period (2020-08-01) until (2021-00-00)
+		days_before = chat.df[(chat.df.date<= '2020-08-01')].index
+		aux_chat = chat.df.drop(days_before)
+		days_after = aux_chat[(aux_chat.date>= '2021-01-01')].index
+		chat_df = aux_chat.drop(days_after)
 
-        # Total messages
-        total_messages = len(chat_df)
-        print('Total Messages: ', total_messages)
+		# Total messages
+		total_messages = len(chat_df)
+		print('Total Messages: ', total_messages)
 
-        # Average messages
-        message_lengths=[len(x) for x in chat_df.message]
-        average_length = sum(message_lengths)/len(message_lengths)
-        print('Average length: ', average_length)
+		# Average messages
+		message_lengths=[len(x) for x in chat_df.message]
+		average_length = sum(message_lengths)/len(message_lengths)
+		print('Average length: ', average_length)
 
-        # Emoji count and top emojis
-        # Note: some emojis (e.g. ▪) should not be counted
-        emoji_count = 0
-        emojis = dict.fromkeys(
-            [' ' * i for i in range(0,10)],
-         0)
-        for m in tqdm(chat.df.message):
-            emojis_message = utils.count_emojis(m)
-            emoji_count += len(emojis_message)
-            for e in emojis_message:
-                if e not in emojis:
-                    emojis[e] = 0
-                emojis[e] +=1
-        print('Emoji count: ', emoji_count)
-        top10_emojis = sorted(emojis, key=emojis.get, reverse=True)[:10]
-        top10_emojis_with_value = [[x, emojis[x]] for x in top10_emojis]
-        print('Top emojis: ', top10_emojis_with_value)
+		# Emoji count and top emojis
+		# Note: some emojis (e.g. ▪) should not be counted
+		emoji_count = 0
+		emojis = dict.fromkeys(
+			[' ' * i for i in range(0,10)],
+		 0)
+		for m in tqdm(chat.df.message):
+			emojis_message = utils.count_emojis(m)
+			emoji_count += len(emojis_message)
+			for e in emojis_message:
+				if e not in emojis:
+					emojis[e] = 0
+				emojis[e] +=1
+		print('Emoji count: ', emoji_count)
+		top10_emojis = sorted(emojis, key=emojis.get, reverse=True)[:10]
+		top10_emojis_with_value = [[x, emojis[x]] for x in top10_emojis]
+		print('Top emojis: ', top10_emojis_with_value)
 
-        # Message frequency per hour
-        hours = [0] * 24
-        for d in chat_df.date:
-            hours[d.hour]+=1
-        print('Message frequency per hour: ', hours)
-        
-        # Message frequency per month
-        months = [0] * 12
-        for m in chat_df.date:
-            months[m.month - 1]+=1
-        print('Message frequency per month: ', months)
+		# Message frequency per hour
+		hours = [0] * 24
+		for d in chat_df.date:
+			hours[d.hour]+=1
+		print('Message frequency per hour: ', hours)
+		
+		# Message frequency per month
+		months = [0] * 12
+		for m in chat_df.date:
+			months[m.month - 1]+=1
+		print('Message frequency per month: ', months)
 
-        # Message frequency per day of the week
-        week_days = [0] * 7
-        for w in chat_df.date:
-            week_days[w.dayofweek]+=1
-        print('Message frequency per day of the week: ', week_days)
+		# Message frequency per day of the week
+		week_days = [0] * 7
+		for w in chat_df.date:
+			week_days[w.dayofweek]+=1
+		print('Message frequency per day of the week: ', week_days)
 
-        # Line break analysis
-        total_breaks = 0
-        for m in chat_df.message:
-            total_breaks += len(m.split('\n'))
-        print('Total breaks: ',total_breaks)
+		# Line break analysis
+		total_breaks = 0
+		for m in chat_df.message:
+			total_breaks += len(m.split('\n'))
+		print('Total breaks: ',total_breaks)
 
-        # Exclamation mark analysis
-        total_exclamations = 0
-        for m in chat_df.message:
-            total_exclamations += len([x for x in m if x=='!'])
-        print('Total exclamation marks: ',total_exclamations)
+		# Exclamation mark analysis
+		total_exclamations = 0
+		for m in chat_df.message:
+			total_exclamations += len([x for x in m if x=='!'])
+		print('Total exclamation marks: ',total_exclamations)
 
-        # Interrogation mark analysis
-        total_interrogations = 0
-        for m in chat_df.message:
-            total_interrogations += len([x for x in m if x=='?'])
-        print('Total interrogation marks: ',total_interrogations)
+		# Interrogation mark analysis
+		total_interrogations = 0
+		for m in chat_df.message:
+			total_interrogations += len([x for x in m if x=='?'])
+		print('Total interrogation marks: ',total_interrogations)
 
-        # Count capital letters
-        total_capital = 0
-        for m in chat_df.message:
-            total_capital += len([x for x in m if x.isupper()])
-        print('Total capital letters: ',total_capital)
+		# Count capital letters
+		total_capital = 0
+		for m in chat_df.message:
+			total_capital += len([x for x in m if x.isupper()])
+		print('Total capital letters: ',total_capital)
 
-        # Count links
-        total_links = 0
-        for m in chat_df.message:
-            words = m.split()
-            total_links += len([x for x in words if 'http' in x])
-        print('Total links: ',total_links)
+		# Count links
+		total_links = 0
+		for m in chat_df.message:
+			words = m.split()
+			total_links += len([x for x in words if 'http' in x])
+		print('Total links: ',total_links)
 
-        # Count video
-        total_videos = 0
-        for m in chat_df.message:
-            words = m.split()
-            total_videos += len([x for x in words if '.mp4>' in x])
-        print('Total videos: ',total_videos)
+		# Count video
+		total_videos = 0
+		for m in chat_df.message:
+			words = m.split()
+			total_videos += len([x for x in words if '.mp4>' in x])
+		print('Total videos: ',total_videos)
 
-        # Count images
-        total_images = 0
-        for m in chat_df.message:
-            words = m.split()
-            total_images += len([x for x in words if '.png>' in x or '.jpg>' in x or '.jpeg>' in x])
-        print('Total images: ',total_images)
+		# Count images
+		total_images = 0
+		for m in chat_df.message:
+			words = m.split()
+			total_images += len([x for x in words if '.png>' in x or '.jpg>' in x or '.jpeg>' in x])
+		print('Total images: ',total_images)
 
-        
+		# Sentiment analysis
+		for m in chat_df.message:
+			# Only analyze long sentences
+			for l in m.split('\n'):
+				for s in l.split('.'):
+					if len(s) > MIN_SENTENCE:
+						emotion = Text(s)
+						import pdb;pdb.set_trace()
+				sentences_raw = [x for x in sentences_dot if len(x) > MIN_SENTENCE]
+				sentences.extend(sentences_raw)
+		
 
 
-        # TODO First word analysis
-        # TODO Common words analysis
-        # TODO Emoji: country flags used
+		# TODO First word analysis
+		# TODO Common words analysis
+		# TODO Emoji: country flags used
 
-        # TODO Sentiment analysis: PCA representation in clusters
-        # TODO Sentiment analysis positive/negative & subjective/objective (PT!)
-        
-                
-        csvf.writerow([
-            chat_name, 
-            total_messages, 
-            average_length, 
-            emoji_count, 
-            chat.start_date, chat.end_date] +
-            hours +
-            months +
-            week_days +
-            [total_breaks] +
-            [total_exclamations] +
-            [total_interrogations] +
-            [total_capital] +
-            [total_links] +
-            [total_videos] +
-            [total_images] +
-            [top10_emojis_with_value[0][0]] +
-            [top10_emojis_with_value[0][1]] +
-            [top10_emojis_with_value[1][0]] +
-            [top10_emojis_with_value[1][1]] +
-            [top10_emojis_with_value[2][0]] +
-            [top10_emojis_with_value[2][1]] +
-            [top10_emojis_with_value[3][0]] +
-            [top10_emojis_with_value[3][1]] +
-            [top10_emojis_with_value[4][0]] +
-            [top10_emojis_with_value[4][1]] +
-            [top10_emojis_with_value[5][0]] +
-            [top10_emojis_with_value[5][1]] +
-            [top10_emojis_with_value[6][0]] +
-            [top10_emojis_with_value[6][1]] +
-            [top10_emojis_with_value[7][0]] +
-            [top10_emojis_with_value[7][1]] +
-            [top10_emojis_with_value[8][0]] +
-            [top10_emojis_with_value[8][1]] +
-            [top10_emojis_with_value[9][0]] +
-            [top10_emojis_with_value[9][1]]
-            )
+		# TODO Sentiment analysis: PCA representation in clusters
+		# TODO Sentiment analysis positive/negative & subjective/objective (PT!)
+		
+				
+		csvf.writerow([
+			chat_name, 
+			total_messages, 
+			average_length, 
+			emoji_count, 
+			chat.start_date, chat.end_date] +
+			hours +
+			months +
+			week_days +
+			[total_breaks] +
+			[total_exclamations] +
+			[total_interrogations] +
+			[total_capital] +
+			[total_links] +
+			[total_videos] +
+			[total_images] +
+			[top10_emojis_with_value[0][0]] +
+			[top10_emojis_with_value[0][1]] +
+			[top10_emojis_with_value[1][0]] +
+			[top10_emojis_with_value[1][1]] +
+			[top10_emojis_with_value[2][0]] +
+			[top10_emojis_with_value[2][1]] +
+			[top10_emojis_with_value[3][0]] +
+			[top10_emojis_with_value[3][1]] +
+			[top10_emojis_with_value[4][0]] +
+			[top10_emojis_with_value[4][1]] +
+			[top10_emojis_with_value[5][0]] +
+			[top10_emojis_with_value[5][1]] +
+			[top10_emojis_with_value[6][0]] +
+			[top10_emojis_with_value[6][1]] +
+			[top10_emojis_with_value[7][0]] +
+			[top10_emojis_with_value[7][1]] +
+			[top10_emojis_with_value[8][0]] +
+			[top10_emojis_with_value[8][1]] +
+			[top10_emojis_with_value[9][0]] +
+			[top10_emojis_with_value[9][1]]
+			)
 
 print('Finished')
