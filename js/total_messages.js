533ab325ef21d259ea29d02d1c188cc7b9bd5169
Enric Moreu <enric.moreu.filella@gmail.com>
2021-04-19 17:33:33 +0200

    Total messages instead of characters

--- js/total_messages.js
+++ js/total_messages.js
@@ -8,16 +8,16 @@ $(document).ready(function () {
     var data_raw = rows.map(function (row) {
       return {
         chat_names: row['Chat  Name'],
-        total_messages: row['Average  Characters  Length']
+        total_messages: row['Total  Messages']
       }
     })
 
     var processed_data = {};
     processed_data['chat_names'] = [];
-    processed_data['avg_characters'] = [];
+    processed_data['total_messages'] = [];
     for (i = 0; i < data_raw.length; i++) {
       processed_data['chat_names'].push(data_raw[i].chat_names.slice(0, -4));
-      processed_data['avg_characters'].push((data_raw[i].total_messages * 1).toFixed(0));
+      processed_data['total_messages'].push((data_raw[i].total_messages * 1).toFixed(0));
     }
 
     new Chart(document.getElementById("total_messages"), {
@@ -25,8 +25,8 @@ $(document).ready(function () {
       data: {
         labels: processed_data['chat_names'],
         datasets: [{
-          label: 'Total characters',
-          data: processed_data['avg_characters'],
+          label: 'Total messages',
+          data: processed_data['total_messages'],
           backgroundColor: function (context) {
             var index = context.dataIndex;
             var value = processed_data['chat_names'][index];
@@ -40,7 +40,7 @@ $(document).ready(function () {
         legend: { display: false },
         title: {
           display: true,
-          text: 'Total characters per channel'
+          text: 'Total messages'
         },
         scales: {
           xAxes: [{
