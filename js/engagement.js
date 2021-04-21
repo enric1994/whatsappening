401688441ee42bac0b8880c0c1c9c214b9dd597e
Enric Moreu <enric.moreu.filella@gmail.com>
2021-04-21 22:36:42 +0200

    Engagement + fix indexs

--- js/engagement.js
+++ js/engagement.js
@@ -3,31 +3,16 @@ $(document).ready(function () {
   // Read data file and create a chart
   $.get('./statsV3.csv', function (csvString) {
 
-    // var rows = Papa.parse(csvString, { header: true }).data;
-
-    // var data_raw = rows.map(function (row) {
-    //   return {
-    //     chat_names: row['Chat  Name'],
-    //     total_messages: row['Total  Messages']
-    //   }
-    // })
-
-    // var processed_data = {};
-    // processed_data['chat_names'] = [];
-    // processed_data['total_messages'] = [];
-    // for (i = 0; i < data_raw.length; i++) {
-    //   processed_data['chat_names'].push(data_raw[i].chat_names.slice(0, -4));
-    //   processed_data['total_messages'].push((data_raw[i].total_messages * 1).toFixed(0));
-    // }
-
-    new Chart(document.getElementById("objective_of_messages"), {
+    new Chart(document.getElementById("engagement"), {
       type: 'doughnut',
       data: {
-        labels: ['Engagement only', 'Distribution and engagement', 'Distribution only'],
+        labels: ['Feedback', 'Survey', 'Questions', 'Contact', 'Crowdsourcing', 'Share', 'Promotion'],
+        // labels: ['Crowdsourcing', 'Questions', 'Feedback', 'Promotion', 'Survey', 'Contact', 'Share'],
         datasets: [{
           // label: 'Total messages',
-          data: [1.5,11.3,87.2],
-          backgroundColor: ['#ffd166', '#06d6a0', '#118ab2']
+          data: [1.1, 1.1, 5.7, 6.8, 11.9, 36.4, 36.9],
+          // data: [11.9, 5.7, 1.1, 36.9, 1.1, 6.8, 36.4],
+          backgroundColor: ['#EF476F', '#f78c6b', '#ffd166', '#83d483', '#06d6a0', '#118ab2', '#0c637f']
         }]
       },
       options: {
